2587e32f5143e1de4bbc6301b183114fbfc5dc0f
Jasper <dnjun80081@gmail.com>
2020-01-18 13:59:42 +0800

    init commit

---
+++ Lottery.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Scanner;
+
+public class Lottery {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("不喜歡的數字:");
+		int a = sc.nextInt();
+		int b = 0;
+		for(int c =0;c <=4; c++) {if(c==a) {continue;}
+		for(int d =0;d <=9;d++) {if(d==a) {continue;}
+		else if(d==0 && c==0) {continue;}
+		System.out.print(c+""+d+"\t");
+			b++;}System.out.println();
+			}
+		System.out.println("總共有"+b+"個可選");
+	}
+  }
+
